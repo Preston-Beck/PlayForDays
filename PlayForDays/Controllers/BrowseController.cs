7d25394fea90b9a8cf737fcef8f409cab5114880
Preston-Beck <pgbeck@lakeheadu.ca>
2021-11-09 18:31:37 -0500

    Added BrowseController

---
+++ PlayForDays/Controllers/BrowseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlayForDays.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayForDays.Controllers
+{
+    public class BrowseController : Controller
+    {
+        //DB Context connection object
+        private readonly ApplicationDbContext _context;
+
+        public BrowseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
